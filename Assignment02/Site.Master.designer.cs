0f92c7c43e1bd1f6316ed1e9c32084b49b7c0af1
Waynell Lovell <wlovell2@my.centennialcollege.ca>
2016-10-07 21:12:48 -0400

    made correct control and updated document

--- Assignment02/Site.Master.designer.cs
+++ Assignment02/Site.Master.designer.cs
@@ -49,6 +49,15 @@ namespace Assignment02 {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl register;
         
         /// <summary>
+        /// lblUsername control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsername;
+        
+        /// <summary>
         /// Usernametxt control.
         /// </summary>
         /// <remarks>
@@ -65,6 +74,15 @@ namespace Assignment02 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox Passwordtxt;
+        
+        /// <summary>
+        /// LoginButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button LoginButton;
         
         /// <summary>
         /// MainContent control.
